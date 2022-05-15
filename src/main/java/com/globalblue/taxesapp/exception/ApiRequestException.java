ca109cc28e5d59b17abbf2d7a1f7d3f34bd1332e
ratek20 <my_email>
2022-05-15 10:23:37 +0200

    Custom Exception

---
+++ src/main/java/com/globalblue/taxesapp/exception/ApiRequestException.java
@@ -0,0 +1,7 @@
+package com.globalblue.taxesapp.exception;
+
+public class ApiRequestException extends RuntimeException{
+    public ApiRequestException(String message) {
+        super(message);
+    }
+}
